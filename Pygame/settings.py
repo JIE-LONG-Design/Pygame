4eded8592caa980cfc088aa6266cd364a1a50626
JIE-LONG-Design <130468675+JIE-LONG-Design@users.noreply.github.com>
2023-04-19 22:45:55 +0200

    Add files via upload

--- Pygame/settings.py
+++ Pygame/settings.py
@@ -20,6 +20,8 @@ class Settings():
         self.fleet_drop_speed = 10
 
         self.speedup_scale = 1.1
+        self.score_scale = 1.5
+
         self.initialize_dynamic_settings()
 
         # fleet_direction of 1 represents right; -1 represents left.
@@ -29,12 +31,14 @@ class Settings():
         self.bullet_speed_factor = 3
         self.alien_speed_factor = 1
         self.fleet_direction = 1
+        self.alien_points = 50
 
     def increase_speed(self):
 
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
-        
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
 
 
